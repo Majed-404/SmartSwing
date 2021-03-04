4aefe163082b188d4b192f0a2a5a7d41e82382c1
engsamarnasr <engsamarnasr@hotmail.com>
2021-03-04 06:17:58 +0200

    complete user group

--- Angular/src/app/admin/menu-items/groups/group-dialog/group-dialog.component.ts
+++ Angular/src/app/admin/menu-items/groups/group-dialog/group-dialog.component.ts
@@ -1,7 +1,9 @@
+import { G } from '@angular/cdk/keycodes';
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Group } from 'src/app/app.models';
+import { UserManagemntService } from '../../user-managemnt.service';
 
 @Component({
   selector: 'app-group-dialog',
@@ -12,13 +14,13 @@ export class GroupDialogComponent implements OnInit {
   public form: FormGroup;
   constructor(public dialogRef: MatDialogRef<GroupDialogComponent>,
               @Inject(MAT_DIALOG_DATA) public group: Group,
-              public fb: FormBuilder) { }
+              public fb: FormBuilder,
+              public userMangmentService: UserManagemntService) { }
 
   ngOnInit(): void { 
     this.form = this.fb.group({
       id: 0,
-      name: [null, Validators.required],
-      description: null 
+      name: [null, Validators.required]
     }); 
 
     if(this.group){
@@ -27,7 +29,9 @@ export class GroupDialogComponent implements OnInit {
   }
 
   public onSubmit(){ 
+    debugger
     if(this.form.valid){
+      this.userMangmentService.addGroup(this.form.value);
       this.dialogRef.close(this.form.value);
     }
   }
