5bdbc81e55d8ef9f2fd6dfe9f59b429064460c62
Mahmoud <mahmoudaltayyeb1@outlook.com>
2021-04-13 18:47:13 +0200

    charts

---
+++ Angular/src/app/pages/stocks/stocks.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-stocks',
+  templateUrl: './stocks.component.html',
+  styleUrls: ['./stocks.component.scss'],
+  // encapsulation: ViewEncapsulation.ShadowDom
+})
+export class StocksComponent implements OnInit {
+
+
+  
+  // public toggleSearchBar:boolean = false;
+  public myControl = new FormControl();
+  public id:number;
+
+
+
+  constructor(public router: Router,private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    debugger
+    // this.id = this.route.snapshot.params['id'];
+    
+  }
+
+}
