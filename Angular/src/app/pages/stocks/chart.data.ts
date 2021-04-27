8c6f67e32cb8bc583f7ca2be5923b19e2e518df9
Mahmoud <mahmoudaltayyeb1@outlook.com>
2021-04-27 15:19:14 +0200

    angular-gson pushed

--- Angular/src/app/pages/stocks/chart.data.ts
+++ Angular/src/app/pages/stocks/chart.data.ts
@@ -1,290 +1,189 @@
-export const orders = [
-    {
-     name: 'Orders',
-     series: [
-       {
-         name: "1980",
-         value: 21632
-       }
-     ]
-   }
- ]
- 
- export const products = [
-   {
-     "name": "Product-1",
-     "value": 69400
-   },
-   {
-     "name": "Product-2",
-     "value": 59400
-   },
-   {
-     "name": "Product-3",
-     "value": 82400
-   },
-   {
-     "name": "Product-4",
-     "value": 73400
-   },
-   {
-     "name": "Product-5",
-     "value": 25400
-   },
-   {
-     "name": "Product-6",
-     "value": 23400
-   },
-   {
-     "name": "Product-7",
-     "value": 49300
-   },
-   {
-     "name": "Product-8",
-     "value": 55400
-   },
-   {
-     "name": "Product-9",
-     "value": 37400
-   },
-   {
-     "name": "Product-10",
-     "value": 65220
-   },
-   {
-     "name": "Product-11",
-     "value": 79400
-   },
-   {
-     "name": "Product-12",
-     "value": 58400
-   },
-   {
-     "name": "Product-13",
-     "value": 41400
-   },
-   {
-     "name": "Product-14",
-     "value": 37400
-   },
-   {
-     "name": "Product-15",
-     "value": 33700
-   },
-   {
-     "name": "Product-16",
-     "value": 42700
-   },
-   {
-     "name": "Product-17",
-     "value": 52700
-   },
-   {
-     "name": "Product-18",
-     "value": 62700
-   }
- ]
- 
- export const customers = [
-    {
-     name: 'Customers',
-     series: [
-       {
-         name: "2000",
-         value: 34502
-       }
-     ]
-   }
- ]
- 
- export const refunds = [
-   {
-     "name": "Item-1",
-     "value": 23701
-   },
-   {
-     "name": "Item-2",
-     "value": 33701
-   },
-   {
-     "name": "Item-3",
-     "value": 63701
-   },
-   {
-     "name": "Item-4",
-     "value": 52701
-   },
-   {
-     "name": "Item-5",
-     "value": 73701
-   },
-   {
-     "name": "Item-6",
-     "value": 43701
-   },
-   {
-     "name": "Item-7",
-     "value": 83701
-   },
-   {
-     "name": "Item-8",
-     "value": 29701
-   },
-   {
-     "name": "Item-9",
-     "value": 69701
-   },
-   {
-     "name": "Item-10",
-     "value": 58701
-   },
-   {
-     "name": "Item-11",
-     "value": 65701
-   },
-   {
-     "name": "Item-12",
-     "value": 47701
-   },
-   {
-     "name": "Item-13",
-     "value": 41701
-   },
-   {
-     "name": "Item-14",
-     "value": 25701
-   },
-   {
-     "name": "Item-15",
-     "value": 35701
-   }
- ]
- 
- export const montly_sales = [
-   {
-     "name": "Offline Stores",
-     "value": 178
-   },
-   {
-     "name": "Digital Stores",
-     "value": 340
-   },
-   {
-     "name": "Others",
-     "value": 280
-   }
- ]
- 
- export const analytics = [
-   {
-     name: 'Company 1',
-     series: [
-       {
-         name: '2010',
-         value: 31632
-       },
-       {
-         name: '2011',
-         value: 42589
-       },
-       {
-         name: '2012',
-         value: 52458
-       },
-       {
-         name: '2013',
-         value: 69632
-       },
-       {
-         name: '2014',
-         value: 52305
-       },
-       {
-         name: '2015',
-         value: 72412
-       },
-       {
-         name: '2016',
-         value: 66285
-       },
-       {
-         name: '2017',
-         value: 49855
-       }
-     ]
-   },
-   {
-     name: 'Company 2',
-     series: [
-       {
-         name: '2010',
-         value: 61632
-       },
-       {
-         name: '2011',
-         value: 68589
-       },
-       {
-         name: '2012',
-         value: 55458
-       },
-       {
-         name: '2013',
-         value: 62632
-       },
-       {
-         name: '2014',
-         value: 38305
-       },
-       {
-         name: '2015',
-         value: 41412
-       },
-       {
-         name: '2016',
-         value: 32285
-       },
-       {
-         name: '2017',
-         value: 31855
-       }
-     ]
-   },
-   {
-     name: 'Company 3',
-     series: [
-       {
-         name: '2010',
-         value: 55632
-       },
-       {
-         name: '2011',
-         value: 63589
-       },
-       {
-         name: '2012',
-         value: 70458
-       },
-       {
-         name: '2013',
-         value: 79632
-       },
-       {
-         name: '2014',
-         value: 59305
-       },
-       {
-         name: '2015',
-         value: 56412
-       },
-       {
-         name: '2016',
-         value: 49285
-       },
-       {
-         name: '2017',
-         value: 38855
-       }
-     ]
-   }
- ]
+export const seriesData = [
+  {
+    x: new Date(2016, 1, 1),
+    y: [51.98, 56.29, 51.59, 53.85]
+  },
+  {
+    x: new Date(2016, 2, 1),
+    y: [53.66, 54.99, 51.35, 52.95]
+  }, 
+  {
+    x: new Date(2016, 3, 1),
+    y: [52.96, 53.78, 51.54, 52.48]
+  },
+  {
+    x: new Date(2016, 4, 1),
+    y: [52.54, 52.79, 47.88, 49.24]
+  },
+  {
+    x: new Date(2016, 5, 1),
+    y: [49.1, 52.86, 47.7, 52.78]
+  },
+  {
+    x: new Date(2016, 6, 1),
+    y: [52.83, 53.48, 50.32, 52.29]
+  },
+  {
+    x: new Date(2016, 7, 1),
+    y: [52.2, 54.48, 51.64, 52.58]
+  },
+  {
+    x: new Date(2016, 8, 1),
+    y: [52.76, 57.35, 52.15, 57.03]
+  },
+  {
+    x: new Date(2016, 9, 1),
+    y: [57.04, 58.15, 48.88, 56.19]
+  },
+  {
+    x: new Date(2016, 10, 1),
+    y: [56.09, 58.85, 55.48, 58.79]
+  },
+  {
+    x: new Date(2016, 11, 1),
+    y: [58.78, 59.65, 58.23, 59.05]
+  },
+  {
+    x: new Date(2017, 0, 1),
+    y: [59.37, 61.11, 59.35, 60.34]
+  },
+  {
+    x: new Date(2017, 1, 1),
+    y: [60.4, 60.52, 56.71, 56.93]
+  },
+  {
+    x: new Date(2017, 2, 1),
+    y: [57.02, 59.71, 56.04, 56.82]
+  },
+  {
+    x: new Date(2017, 3, 1),
+    y: [56.97, 59.62, 54.77, 59.3]
+  },
+  {
+    x: new Date(2017, 4, 1),
+    y: [59.11, 62.29, 59.1, 59.85]
+  },
+  {
+    x: new Date(2017, 5, 1),
+    y: [59.97, 60.11, 55.66, 58.42]
+  },
+  {
+    x: new Date(2017, 6, 1),
+    y: [58.34, 60.93, 56.75, 57.42]
+  },
+  {
+    x: new Date(2017, 7, 1),
+    y: [57.76, 58.08, 51.18, 54.71]
+  },
+  {
+    x: new Date(2017, 8, 1),
+    y: [54.8, 61.42, 53.18, 57.35]
+  },
+  {
+    x: new Date(2017, 9, 1),
+    y: [57.56, 63.09, 57.0, 62.99]
+  },
+  {
+    x: new Date(2017, 10, 1),
+    y: [62.89, 63.42, 59.72, 61.76]
+  },
+  {
+    x: new Date(2017, 11, 1),
+    y: [61.71, 64.15, 61.29, 63.04]
+  }
+];
+
+export const seriesDataLinear = [
+  {
+    x: new Date(2016, 1, 1),
+    y: 3.85
+  },
+  {
+    x: new Date(2016, 2, 1),
+    y: 2.95
+  },
+  {
+    x: new Date(2016, 3, 1),
+    y: -12.48
+  },
+  {
+    x: new Date(2016, 4, 1),
+    y: 19.24
+  },
+  {
+    x: new Date(2016, 5, 1),
+    y: 12.78
+  },
+  {
+    x: new Date(2016, 6, 1),
+    y: 22.29
+  },
+  {
+    x: new Date(2016, 7, 1),
+    y: -12.58
+  },
+  {
+    x: new Date(2016, 8, 1),
+    y: -17.03
+  },
+  {
+    x: new Date(2016, 9, 1),
+    y: -19.19
+  },
+  {
+    x: new Date(2016, 10, 1),
+    y: -28.79
+  },
+  {
+    x: new Date(2016, 11, 1),
+    y: -39.05
+  },
+  {
+    x: new Date(2017, 0, 1),
+    y: 20.34
+  },
+  {
+    x: new Date(2017, 1, 1),
+    y: 36.93
+  },
+  {
+    x: new Date(2017, 2, 1),
+    y: 36.82
+  },
+  {
+    x: new Date(2017, 3, 1),
+    y: 29.3
+  },
+  {
+    x: new Date(2017, 4, 1),
+    y: 39.85
+  },
+  {
+    x: new Date(2017, 5, 1),
+    y: 28.42
+  },
+  {
+    x: new Date(2017, 6, 1),
+    y: 37.42
+  },
+  {
+    x: new Date(2017, 7, 1),
+    y: 24.71
+  },
+  {
+    x: new Date(2017, 8, 1),
+    y: 37.35
+  },
+  {
+    x: new Date(2017, 9, 1),
+    y: 32.99
+  },
+  {
+    x: new Date(2017, 10, 1),
+    y: 31.76
+  },
+  {
+    x: new Date(2017, 11, 1),
+    y: 43.04
+  }
+];
