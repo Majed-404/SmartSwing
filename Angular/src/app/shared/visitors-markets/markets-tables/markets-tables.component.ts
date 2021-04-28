440424596f0979fd4104a48eae40cab7d39e6ca6
engsamarnasr <engsamarnasr@hotmail.com>
2021-04-28 15:06:27 -0700

    visitors stocks

--- Angular/src/app/shared/visitors-markets/markets-tables/markets-tables.component.ts
+++ Angular/src/app/shared/visitors-markets/markets-tables/markets-tables.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { Stocks } from 'src/app/pages/account/stocks/stockes.model';
+import { VisitorMarketService } from '../visitor-market-.service';
 
 @Component({
   selector: 'app-markets-tables',
@@ -11,34 +13,31 @@ import { MatTableDataSource } from '@angular/material/table';
 })
 export class MarketsTablesComponent implements OnInit {
 
-  public displayedColumns = ['position', 'name', 'weight', 'symbol', 'index'];
+  public displayedColumns = ['nmId', 'nmSymbol', 'nmEnName', 'nmArName', 'nmMktId',
+   'nmStopFlag', 'dtNmId', 'dtDt', 'dtOpen', 'dtLow', 'dtHigh', 'dtClose', 'dtVol', 'dtMktId', 'dtValue', 'dtNvalue'];
+   
   public dataSource : MatTableDataSource<Element> ;
-  public url = 'https://financialmodelingprep.com/api/v3/quote/AAPL?apikey=demo';
+  public stocksList: Stocks[];
+  //public url = "http://localhost:49974/api/Markets/StockListExchange/{Id}";
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
-  constructor(private http: HttpClient) {
-    this.http.get<any>(this.url).subscribe(data => {
-     
-    })
+  constructor(private http: HttpClient,private _VisitorMarketService:VisitorMarketService) {
+    
   }
 
   ngOnInit() {
 
-    this.getGroupList();
-    
-    setTimeout(() => {
-      this.dataSource.paginator = this.paginator;
-      this.dataSource.sort = this.sort;
-    },1000);
+    this.getStocksList();
     
   } 
 
-  public getGroupList(){
-    // this.userManagemntService.getGroups().subscribe(response => {
-    //   this.groups=<Group[]>response;
-    //   this.initDataSource(this.groups); 
-    // })
+  public getStocksList(){
+    debugger
+    this._VisitorMarketService.getVisitorStocks(2).subscribe(data => {
+     this.stocksList = <Stocks[]>data;
+     this.initDataSource(this.stocksList)
+    });
   }
 
   public initDataSource(data:any){
@@ -50,34 +49,3 @@ export class MarketsTablesComponent implements OnInit {
   
 }
 
-
-
-export interface Element {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: Element[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
-  {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
-  {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
-  {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
-  {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
-  {position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
-  {position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
-  {position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
-  {position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
-  {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
-];
---
+++ Angular/src/app/shared/visitors-markets/stocks.model.ts
@@ -0,0 +1,19 @@
+export class Stocks{
+
+    "nmId": number;
+    "nmSymbol": string;
+    "nmEnName": string;
+    "nmArName": string;
+    "nmMktId": string;
+    "nmStopFlag": boolean;
+    "dtNmId": number;
+    "dtDt": string;
+    "dtOpen": number;
+    "dtLow": number;
+    "dtHigh": number;
+    "dtClose": number;
+    "dtVol": number;
+    "dtMktId": number;
+    "dtValue": number;
+    "dtNvalue": number;
+}
